27e5deda3e32311bc61e465f44cae228fdee0588
vadimtaop <vadimmuhin77@gmail.com>
2026-03-04 22:07:19 +0300

    Начало проекта

---
+++ CinemaPremiereApp/CinemaPremiereApp/Pages/AuthPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CinemaPremiereApp.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthPage.xaml
+    /// </summary>
+    public partial class AuthPage : Page
+    {
+        public AuthPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButtonClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string login = LoginTextBox.Text;
+                string password = PasswordTextBox.Password;
+
+                if (string.IsNullOrEmpty(login))
+                {
+                    MainSnackbar.MessageQueue.Enqueue(
+                        content:"Ошибка\nВведите данные в поле 'Логин'",
+                        null, null, null, false, true,
+                        durationOverride: TimeSpan.FromSeconds(3));
+                    return;
+                }
+                if (string.IsNullOrEmpty(password))
+                {
+                    MainSnackbar.MessageQueue.Enqueue(
+                        content: "Ошибка\nВведите данные в поле 'Пароль'",
+                        null, null, null, false, true,
+                        durationOverride: TimeSpan.FromSeconds(3));
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
---
+++ CinemaPremiereApp/CinemaPremiereApp/Windows/GeneralWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CinemaPremiereApp.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для GeneralWindow.xaml
+    /// </summary>
+    public partial class GeneralWindow : Window
+    {
+        public GeneralWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ThemeToggleButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (sender is ToggleButton toggleButton)
+            {
+                PaletteHelper paletteHelper = new PaletteHelper();
+
+                var theme = paletteHelper.GetTheme();
+
+                bool isDark = toggleButton.IsChecked ?? false;
+
+                theme.SetBaseTheme(isDark ? BaseTheme.Dark : BaseTheme.Light);
+
+                paletteHelper.SetTheme(theme);
+            }
+        }
+    }
+}
